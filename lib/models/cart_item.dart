a9c10b2a0d286a1e0dfee3bf74b5942ce1efb5c7
Alston Sahyun Kim <sahyun1@hotmail.com>
2021-07-25 12:36:52 +1200

    added cart model and provider

---
+++ lib/models/cart_item.dart
@@ -0,0 +1,8 @@
+class CartItem {
+  final String id;
+  final String title;
+  final int qunatity;
+  final double price;
+
+  CartItem(this.id, this.title, this.qunatity, this.price);
+}
---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_complete_guide/models/cart_item.dart';
+
+class CartProvider with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {...items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(productId, (existingItem) => CartItem(existingItem.id, existingItem.title, existingItem.qunatity + 1, existingItem.price));
+    } else {
+      _items.putIfAbsent(productId,
+          () => CartItem(DateTime.now().toString(), title, 1, price));
+    }
+  }
+}
