967344c7fbd825ff750e329f760f75ebbf1fdad1
Alston Sahyun Kim <sahyun1@hotmail.com>
2021-08-05 22:51:09 +1200

    added logging in and sign up using firebase auth rest api

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/providers/auth_provider.dart';
 import 'package:flutter_complete_guide/screens/add_edit_product_screen.dart';
+import 'package:flutter_complete_guide/screens/auth_screen.dart';
 import 'package:flutter_complete_guide/screens/orders_screen.dart';
-import 'package:flutter_complete_guide/screens/sign_in_screen.dart';
 import 'package:flutter_complete_guide/screens/user_products_screen.dart';
 import 'providers/cart_provider.dart';
 import 'providers/order_provider.dart';
 import 'screens/cart_screen.dart';
 import 'package:provider/provider.dart';
 import '/screens/product_detail_screen.dart';
-import '/screens/products_overview_screen.dart';
 import 'providers/product_provider.dart';
 
 void main() => runApp(MyApp());
@@ -37,17 +36,17 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.purple,
           accentColor: Colors.deepOrange,
-          inputDecorationTheme: inputDecorationTheme(),
+          // inputDecorationTheme: inputDecorationTheme(),
           fontFamily: 'Lato',
         ),
-        home: SignInScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.ROUTE_NAME: (ctx) => ProductDetailScreen(),
           CartScreen.ROUTE_NAME: (ctx) => CartScreen(),
           OrdersScreen.PAGE_ROUTE: (ctx) => OrdersScreen(),
           UserProductsScreen.ROUTE_NAME: (ctx) => UserProductsScreen(),
           AddEditProductScreen.ROUTE_NAME: (ctx) => AddEditProductScreen(),
-          SignInScreen.ROUTE_NAME: (ctx) => SignInScreen()
+          AuthScreen.routeName: (ctx) => AuthScreen()
         },
       ),
     );
@@ -58,10 +57,16 @@ class MyApp extends StatelessWidget {
         borderRadius: BorderRadius.circular(28),
         borderSide: BorderSide(color: Color(0xFF757575)),
         gapPadding: 10);
+    var errorlineInputBorder = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(28),
+        borderSide: BorderSide(color: Colors.red),
+        gapPadding: 10);
     return InputDecorationTheme(
         floatingLabelBehavior: FloatingLabelBehavior.always,
         contentPadding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
         enabledBorder: outlineInputBorder,
-        focusedBorder: outlineInputBorder);
+        focusedBorder: outlineInputBorder,
+        errorBorder: errorlineInputBorder,
+        focusedErrorBorder: errorlineInputBorder);
   }
 }
--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_complete_guide/http_exception.dart';
 import 'package:http/http.dart' as http;
 
 class AuthProvider with ChangeNotifier {
@@ -8,14 +9,31 @@ class AuthProvider with ChangeNotifier {
   DateTime expireDate;
   String _userId;
 
+  Future<void> _authenticate(String email, String password, url) async {
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+            {'email': email, 'password': password, 'returnSecureToken': true}),
+      );
+
+      if (json.decode(response.body)['error'] != null) {
+        throw HttpException(json.decode(response.body)['error']['message']);
+      }
+    } catch (error) {
+      throw (error);
+    }
+  }
+
   Future<void> signup(String email, String password) async {
     final url = Uri.parse(
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA22YaICaE23DsqTLURqm-Eg66i0Wk2SdE');
+    return _authenticate(email, password, url);
+  }
 
-    final response = await http.post(url,
-        body: json.encode(
-            {'email': email, 'password': password, 'returnSecureToken': true}));
-
-    print(json.decode(response.body));
+  Future<void> signIn(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA22YaICaE23DsqTLURqm-Eg66i0Wk2SdE');
+    return _authenticate(email, password, url);
   }
 }
